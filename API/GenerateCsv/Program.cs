0ee58735495a7b4fae94eaaaee23c0a85cb9b624
ilunnie <luisgustavocaris2004@gmail.com>
2024-03-28 13:29:41 -0300

    re-training

--- API/GenerateCsv/Program.cs
+++ API/GenerateCsv/Program.cs
@@ -1,68 +1,60 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
+using System.IO;
 using ColorMine.ColorSpaces;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
-HttpClient.DefaultProxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
-
-int references = 100;
-long lines = (long)1e6;
+// Number of records that will have in the generated csv
+long records = (long)1e7;
 
 List<object[]> data = new() {
     new string[] { "L", "A", "B" }
 };
 
-List<Image<Rgba32>> images = await RandomImages.RandomImage(references);
-Console.WriteLine("Requisições Finalizadas");
+string folder = Path.Combine(Environment.CurrentDirectory, "img");
+string[] files = Directory.GetFiles(folder, "*.jpg");
+int filesLength = files.Length;
 
-var quantLines = lines / references;
-foreach (var image in images)
+Image<Rgba32>[] images = new Image<Rgba32>[filesLength];
+for (int i = 0; i < filesLength; i++)
 {
-    for (int j = 0; j < quantLines; j++)
+    Console.Clear();
+    Console.WriteLine($"Carregando imagens...");
+    Console.WriteLine($"> {(((i + 1) / (double)filesLength) * 100).ToString("0.##")}% Concluido");
+    using (FileStream stream = new FileStream(files[i], FileMode.Open, FileAccess.Read))
+        images[i] = Image.Load<Rgba32>(stream);
+}
+
+var refs = records / filesLength;
+for (int i = 0; i < filesLength; i++)
+{
+    Console.Clear();
+    Console.WriteLine("Imagens Carregadas!");
+    Console.WriteLine();
+    Console.WriteLine("Obtendo dados...");
+    Console.WriteLine($"> {(((i + 1) / (double)filesLength) * 100).ToString("0.##")}% Concluido");
+    for (int j = 0; j < refs; j++)
     {
-        await Task.Run(() =>
-        {
-            Rgba32 pixelColor = image[
-                Random.Shared.Next(image.Width),
-                Random.Shared.Next(image.Height)
-            ];
-            byte r = pixelColor.R;
-            byte g = pixelColor.G;
-            byte b = pixelColor.B;
+        int x = Random.Shared.Next(images[i].Width);
+        int y = Random.Shared.Next(images[i].Height);
+        Rgba32 pixel = images[i][x, y];
 
-            var color = new Rgb { R = r, G = g, B = b };
-            var lab = color.To<Lab>();
+        byte r = pixel.R;
+        byte g = pixel.G;
+        byte b = pixel.B;
 
-            data.Add(new object[] { lab.L, lab.A, lab.B });
-        });
+        var color = new Rgb { R = r, G = g, B = b };
+        var lab = color.To<Lab>();
+
+        data.Add(new object[] { lab.L, lab.A, lab.B });
     }
-};
+}
 
-
-// if (image != null)
-// {
-//     Console.WriteLine($"Dimensões da imagem: {image.Width} x {image.Height}");
-
-//     for (int y = 0; y < image.Height; y++)
-//     {
-//         for (int x = 0; x < image.Width; x++)
-//         {
-//             Rgba32 pixelColor = image[x, y];
-//             byte r = pixelColor.R;
-//             byte g = pixelColor.G;
-//             byte b = pixelColor.B;
-
-//             var L = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-//             byte gray = (byte)Math.Round(L * 255);
-
-//             data.Add(new object[]{r, g, b, gray});
-//         }
-//     }
-// }
-
-Console.WriteLine("Escrevendo o csv...");
+Console.Clear();
+Console.WriteLine("Imagens Carregadas!");
+Console.WriteLine("Dados Obtidos!");
+Console.WriteLine();
+Console.WriteLine("Escrevendo CSV...");
 string filePath = "colorScale.csv";
 Csv.WriteCsv(filePath, data.ToArray());
